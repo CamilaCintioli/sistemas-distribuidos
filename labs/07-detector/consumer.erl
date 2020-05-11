c79cab49e83a4118e940118abdd6523b6c9613f6
camilacintioli <camila.cintioli@gmail.com>
2020-05-11 19:19:16 -0300

    Add monitor to producer in consumer start

--- labs/07-detector/consumer.erl
+++ labs/07-detector/consumer.erl
@@ -7,15 +7,20 @@ start(ProducerPid) ->
     spawn(fun() -> init(ProducerPid) end).
 
 init(ProducerPid) ->
-    ProducerPid ! {hello, self()}, consume(0).
+    Monitor = monitor(process, ProducerPid),
+    ProducerPid ! {hello, self()},
+    consume(0, Monitor).
 
-consume(M) ->
+consume(M,Monitor) ->
     receive
       {ping, M} ->
 	  io:format("El valor esperado es ~w~n", [M]),
-      consume(M+1);
+      consume(M+1,Monitor);
       {ping, N} when N>M -> io:format("Warning~n", []),
-      consume(N+1);
+      consume(N+1,Monitor);
+      {'DOWN', Monitor, process, Object, Info} ->
+        io:format("~w died; ~w~n", [Object, Info]),
+        consume(M, Monitor);
       bye -> stop()
     end.
 
