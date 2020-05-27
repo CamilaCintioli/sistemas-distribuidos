13e4a13ba53b8d5da33d71ddc83c7253cb229f5f
Camila Cintioli <camila.cintioli@gmail.com>
2020-05-27 17:19:18 -0300

    Added clock in loggeador and pqueue module

--- labs/09-loggy/loggeador.erl
+++ labs/09-loggy/loggeador.erl
@@ -7,16 +7,44 @@ start(Nodes) ->
 stop(Logger) ->
     Logger ! stop.
 
-init(_) ->
-    loop().
+init(Nodes) ->
+    Clock = time:clock(Nodes),
+    Queue = pqueue:new(),
+    loop(Clock, Queue).
 
-loop() ->
+loop(Clock, Queue) ->
     receive
         {log, From, Time, Msg} ->
-            log(From, Time, Msg),
-            loop();
+            % encolar mensaje
+            Queue2 = pqueue:queue(Time, {From, Time, Msg}, Queue)
+
+            % updatear clock
+            Clock2 = time:update(From, Time, Clock),
+
+            % si time is safe, log everithing up to time
+            case time:safe(Time,Clock2) of
+                true ->
+                    Queue3 = log_up_to(Time, Queue2),
+                    loop(Clock2, Queue3);
+                false -> 
+                    loop(Clock2, Queue2)
+            end;
          stop ->
             ok
+    end.
+
+log_up_to(Time, Queue) ->
+    {LowerQueue, UpperQueue} = pqueue:split(Time, Queue)
+    log_queue(LowerQueue),
+    UpperQueue.
+
+log_queue(Queue) ->
+    case pqueue:is_empty(Queue) ->
+        true -> ok;
+        false ->
+            {{From, Time, Msg}, Queue2} = pqueue:dequeue(Queue),
+            log(From, Time, Msg),
+            log_queue(Queue2)
     end.
 
 log(From, Time, Msg) ->
---
+++ labs/09-loggy/pqueue.erl
@@ -0,0 +1,17 @@
+-module(pqueue).
+-export([new/0,queue/3,split/2,is_empty/1,dequeue/1]).
+
+new() ->
+    ok.
+
+queue(Priority, Value, Queue) ->
+    ok.
+
+split(Priority, Queue) ->
+    ok.
+
+is_empty(Queue) ->
+    ok.
+
+dequeue(Queue) ->
+    ok.
